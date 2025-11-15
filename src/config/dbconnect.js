4fa4731c2598090697b03ba603e5098ad2e9bff0
felix7559 <felixdarkdragon43@gmail.com>
2025-11-15 17:47:20 +0530

    first commit

--- src/config/dbconnect.js
+++ src/config/dbconnect.js
@@ -38,7 +38,7 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST,
+    // host: process.env.DB_HOST,
     port: process.env.DB_PORT || 3306,
     dialect: "mysql",
     dialectModule: require("mysql2"),
